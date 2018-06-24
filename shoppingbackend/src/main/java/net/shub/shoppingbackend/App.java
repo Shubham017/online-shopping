22e5dda2a63ab5e2acf28977434df1033edd7a99
shubham <shub567@gmail.com>
2018-06-24 20:17:46 +0530

    Successfully Loaded static resources.

---
+++ shoppingbackend/src/main/java/net/shub/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.shub.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
